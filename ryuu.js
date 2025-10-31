7647fb6868aff24ae9f2607c55f4069639215d99
shaaaiu <212079104+shaaaiu@users.noreply.github.com>
2025-10-31 08:17:55 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ ryuu.js
@@ -0,0 +1,5 @@
+[{"number":"6285929924135","status":"active"},
+{"number":"628983623523","status":"active"},
+{"number":"6289507143171","status":"active"},
+{"number":"6283183457199","status":"active"},
+ {"number":"6285767009581","status":"active"}]
