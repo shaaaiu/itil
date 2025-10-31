3e4964799354e7d4f8f33ad8378987909c5de754
Baileyss <alfatharraby414@gmail.com>
2025-10-31 16:12:45 +0700

    Update ryuu.js

--- ryuu.js
+++ ryuu.js
@@ -1,5 +1,5 @@
 [{"number":"6285929924135","status":"active"},
 {"number":"628983623523","status":"active"},
 {"number":"6289507143171","status":"active"},
-{"number":"6283183457199","status":"active"},
+{"number":"62881011848995","status":"active"},
  {"number":"6288286454369","status":"active"}]
